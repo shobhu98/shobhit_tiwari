fd00b5857756e5e0b4dc73228fbf127eb7e22519
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-05-10 03:19:12 +0530

    Updates

---
+++ precache-manifest.8634e4572b05e5cf17f4bd33b3c05bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6a096d9653fb0b0417732e00d1a3f0",
+    "url": "/shobhit_tiwari/index.html"
+  },
+  {
+    "revision": "a1903f9bef1611143fc6",
+    "url": "/shobhit_tiwari/static/css/main.7ba38caf.chunk.css"
+  },
+  {
+    "revision": "4192221e31669adebdba",
+    "url": "/shobhit_tiwari/static/js/2.bac7a57d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shobhit_tiwari/static/js/2.bac7a57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1903f9bef1611143fc6",
+    "url": "/shobhit_tiwari/static/js/main.8e30f3c0.chunk.js"
+  },
+  {
+    "revision": "c762f66dafcaa8cfe154",
+    "url": "/shobhit_tiwari/static/js/runtime-main.459461c6.js"
+  }
+]);
