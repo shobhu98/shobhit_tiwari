6cba9ee195b9f932d48c1939a2dd331f84679bc4
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-05-10 18:46:10 +0530

    font-change

---
+++ build/precache-manifest.0b89fe7d965004c7076811070359bd90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1f67596ed9fe65e78b75f9fdf93c12",
+    "url": "/shobhit_tiwari/index.html"
+  },
+  {
+    "revision": "59dcfb30a28a655c6f69",
+    "url": "/shobhit_tiwari/static/css/main.7ba38caf.chunk.css"
+  },
+  {
+    "revision": "4192221e31669adebdba",
+    "url": "/shobhit_tiwari/static/js/2.bac7a57d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shobhit_tiwari/static/js/2.bac7a57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59dcfb30a28a655c6f69",
+    "url": "/shobhit_tiwari/static/js/main.41b0e5da.chunk.js"
+  },
+  {
+    "revision": "c762f66dafcaa8cfe154",
+    "url": "/shobhit_tiwari/static/js/runtime-main.459461c6.js"
+  }
+]);
