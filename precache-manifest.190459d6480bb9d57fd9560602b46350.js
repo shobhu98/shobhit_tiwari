b7591c52577635afbaa281ea6afb6270e13dbf7c
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-05-10 20:05:06 +0530

    Updates

--- precache-manifest.190459d6480bb9d57fd9560602b46350.js
+++ precache-manifest.190459d6480bb9d57fd9560602b46350.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "508c927f68df2a6ff69cdfb16acf498f",
+    "revision": "e270d6758cbab7813ea449008ec4ffde",
     "url": "/shobhit_tiwari/index.html"
   },
   {
-    "revision": "dc0dbea9523691b0eb68",
-    "url": "/shobhit_tiwari/static/css/main.29f2e233.chunk.css"
+    "revision": "343f001027be474cb67c",
+    "url": "/shobhit_tiwari/static/css/main.6fa7de75.chunk.css"
   },
   {
     "revision": "4192221e31669adebdba",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/shobhit_tiwari/static/js/2.bac7a57d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc0dbea9523691b0eb68",
-    "url": "/shobhit_tiwari/static/js/main.41b0e5da.chunk.js"
+    "revision": "343f001027be474cb67c",
+    "url": "/shobhit_tiwari/static/js/main.e088e4ff.chunk.js"
   },
   {
     "revision": "c762f66dafcaa8cfe154",
